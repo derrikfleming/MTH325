6ba7d5304fedd4eaa1938326d1b277aa89f3f0cc
derrikf2 <fleminde@mail.gvsu.edu>
2017-12-04 13:37:52 -0500

    bleh

--- Project/isomorphisms.py
+++ Project/isomorphisms.py
@@ -7,25 +7,24 @@ def is_same(list1, list2):
                 is_same = False
     else:
         is_same = False
-
     return is_same
 
 #same size, same order
-list1 = ["A", "B", "C"]
-list2 = ["A", "B", "C"]
-print(is_same(list1,list2)) #True
+#list1 = ["A", "B", "C"]
+#list2 = ["A", "B", "C"]
+#print(is_same(list1,list2)) #True
 
 #same size, different order
-list1 = ["A", "B", "C"]
-list2 = ["B", "C", "A"]
-print(is_same(list1,list2)) #True
+#list1 = ["A", "B", "C"]
+#list2 = ["B", "C", "A"]
+#print(is_same(list1,list2)) #True
 
 #different size, different contents
-list1 = ["A", "B", "C"]
-list2 = ["B", "C", "A", "D"]
-print(is_same(list1,list2)) #False
+#list1 = ["A", "B", "C"]
+#list2 = ["B", "C", "A", "D"]
+#print(is_same(list1,list2)) #False
 
 #same size, different contents
-list1 = ["A", "B", "C"]
-list2 = ["D", "E", "F"]
-print(is_same(list1,list2)) #False
+#list1 = ["A", "B", "C"]
+#list2 = ["D", "E", "F"]
+#print(is_same(list1,list2)) #False
