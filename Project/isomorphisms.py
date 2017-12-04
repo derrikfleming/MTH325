5c1f39b4eefe0d13807304818e95f47bc47365e3
derrikf2 <fleminde@mail.gvsu.edu>
2017-12-04 11:20:00 -0500

    is_same additional tests added

--- Project/isomorphisms.py
+++ Project/isomorphisms.py
@@ -10,8 +10,22 @@ def is_same(list1, list2):
 
     return is_same
 
-
+#same size, same order
 list1 = ["A", "B", "C"]
-list2 = ["B", "C", "D"]
+list2 = ["A", "B", "C"]
+print(is_same(list1,list2)) #True
 
-print(is_same(list1,list2))
+#same size, different order
+list1 = ["A", "B", "C"]
+list2 = ["B", "C", "A"]
+print(is_same(list1,list2)) #True
+
+#different size, different contents
+list1 = ["A", "B", "C"]
+list2 = ["B", "C", "A", "D"]
+print(is_same(list1,list2)) #False
+
+#same size, different contents
+list1 = ["A", "B", "C"]
+list2 = ["D", "E", "F"]
+print(is_same(list1,list2)) #False
