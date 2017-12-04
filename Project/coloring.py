218eefedd4231be6062e65c3d86947d47dcedd11
nathankelderman <nathanjkelderman@hotmail.com>
2017-12-04 09:12:49 -0500

    started greedy

--- Project/coloring.py
+++ Project/coloring.py
@@ -36,6 +36,12 @@ def is_proper_edge(graph):
 					return False
 	return True
 
+def greedy(graph,order):
+	color = {}
+	for vert in order:
+		
+		print (graph[vert])
+
 if __name__ == "__main__":
 	graphEmpty = {}
 	grapha = {"A" : ["B", "C"], "B" : ["A", "C"], "C" : ["A", "B"]}
@@ -46,11 +52,15 @@ if __name__ == "__main__":
 	boolb = is_proper(grapha, colorb)
 	print ("Is_proper: " , boola, boolb)
 
-	graphb = {"A":["B"],"B":["C"],"C":["A"]}
+	graphb = {"A":["B", "C"],"B":["A","C"],"C":["A","B"]}
 	print("Three color: " , three_color(graphb))
 
-	graphc = {"A":["B"],"B":["C"],"C":["A"], "D":["A","B","C"]}
+	graphc = {"A":["B","C","D"],"B":["A","C","D"],"C":["A","B","D"], "D":["A","B","C"]}
 	print("Is_three_color: " , is_three_color(graphc))
 
 	g = {"A" : [["B", 1], ["C", 2]], "B" : [["A", 1], ["C", 3]],"C" : [["A", 2], ["B", 3]]}
 	print("Is proper edge: " , is_proper_edge(g))
+
+	ga = {"A":["B", "C"],"B":["A"],"C":["A"]}
+	order = ["A","B","C"]
+	greedy(ga,order)
