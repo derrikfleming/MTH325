3239ddf2b7ca8f5dd2f5d92043ee865804777cb1
derrikf2 <fleminde@mail.gvsu.edu>
2017-12-04 10:58:38 -0500

    edit

--- Project/trees.py
+++ Project/trees.py
@@ -10,12 +10,12 @@
 tree = {"A":["B", "C", "D"],"B":["E","F"], "C" : ["G"], "D" : ["H"], "E" : [], "F" : ["I","J","K"], "G" : [], "H" : [], "I" : [], "J" : [], "K" : []}
 
 def DFS(tree,vertex="A"):
-    visited = []
-    if vertex not in visited:
-        visited += vertex
-        for v in tree[vertex]:
-            visited += DFS(tree,v)
-    return visited
+    dfs_list = []
+    if vertex not in dfs_list:
+        dfs_list += vertex
+        for child in tree[vertex]:
+            dfs_list += DFS(tree,child)
+    return dfs_list
 
 
 print(DFS(tree))
