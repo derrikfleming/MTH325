9a4ddde57a4fcd00737f9ec99c2d1477d91d4547
rushvpayne <roselan@mail.gvsu.edu>
2017-12-07 22:01:15 -0500

    commented

--- Project/Bipartite_Matching.py
+++ Project/Bipartite_Matching.py
@@ -1,8 +1,10 @@
 test1 = ("A","B")
 test2 = ("A","B","C")
-
+"""
+Creates a power set from the list
+"""
 def power(inlist):
-
+    #performs a bitwise opperation
   return [[inlist[j] for j in range(len(inlist))
    if (i&(1<<j))] for i in range(1<<len(inlist))]
 
@@ -12,15 +14,19 @@ def power(inlist):
 def partite_sets(graph):
     left = []
     right =[]
+    #adds the first vertex to the left list
     for a in graph:
         left+= a
         break
     for x in graph:
+        #checks vertices x is connected to and adds them to the right list
         if x in left:
             for y in graph[x]:
                 if y not in right:
                     right += y
         else:
+            #if x insnt in the left list it adds x's connected vertices to the
+            #the left list meaning x is in the right
             for i in graph[x]:
                 if i not in left:
                     left += i
@@ -38,34 +44,40 @@ def partite_sets(graph):
 
 
 def is_perfect(graph):
+    #calls partite set on graph and set graphT to that graph
     graphT = partite_sets(graph)
     left = graphT[0]
     right = graphT[1]
     result = True
 
+
     for a in graph:
         if a in left:
+            #if a vertex a is connected to is not in the right its not perfect
             if graph[a] != right:
                 result = False
         else:
+            #or if its not in the left
             if graph[a] != left:
                 result = False
 
     return result
 
 
-#print (is_perfect(({"A" : ["B", "C"], "B" : ["A","D"], "C" : ["A", "D"], "D" : ["B", "C"]})))
+#print (is_perfect(({"A" : ["B", "C"], "B" : ["A","D"], "C" : ["A", "D"], "D" : ["B","C"]})))
 
 def is_bipartite(graph):
+    #calls partite set on graph and set graphT to that graph
     graphT = partite_sets(graph)
     left = graphT[0]
     right = graphT[1]
     result = True
 
     for a in left:
+        #if a is in the right and the left its not a bipartite
         if a in right:
             result = False
 
     return result
 
-print (is_bipartite({"A" : ["B", "C"], "B" : ["A"], "C" : ["A"]}))
+#print (is_bipartite({"A" : ["B", "C"], "B" : ["A"], "C" : ["A"]}))
